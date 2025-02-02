8b2753804781a6465b7e9c11e866d26b1ee9e400
Nadia Guarracino <nad.guar@gmail.com>
2025-02-02 20:18:35 +0100

    hardcoded all the animations I need

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import s from "./page.module.css";
 // import { Card } from "./ui/Card";
 import LeafIcon from "./icons/leaf";
-import Tree from "./icons/tree";
 import { motion, MotionConfig } from "motion/react";
 
 export default function Home() {
@@ -12,56 +11,125 @@ export default function Home() {
       <div className={s.container}>
         <section className={s.yapSection}>
           {/* Siccome è il nome del sito-startup stilizzalo diversamente */}
-          <h1 className="h1">Emerald Forge</h1>
+          <motion.h1
+            className="h1"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.1 }}
+          >
+            Emerald Forge
+          </motion.h1>
           <br />
-
-          <p className="paragraph">
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2 }}
+          >
             Hi, I am <strong>Nadia G.</strong> from Berlin, a seasoned software
             engineer with 9 years of experience @ Doctolib.
-            <br />
-            <br />I also work on indie apps in my spare time. I love
-            experimenting with new ideas and building tools that solve problems,
-            whether it’s something simple to make my life a little easier or a
-            more complex project that challenges my skills.
-            <br />
-            <br />
+          </motion.p>
+          <br />
+          <br />
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.3 }}
+          >
+            I also work on indie apps in my spare time. I love experimenting
+            with new ideas and building tools that solve problems, whether it’s
+            something simple to make my life a little easier or a more complex
+            project that challenges my skills.
+          </motion.p>
+          <br />
+          <br />
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.4 }}
+          >
             On my blog, I write about everything from{" "}
             <strong>coding tips</strong>, <strong>productivity hacks</strong> to
             reflections on my <strong>job journey</strong> and the{" "}
             <strong>pop culture</strong> that inspires me.
-            <br />
-            <br />I consider myself a <strong>Free Internet Activist</strong>,
+          </motion.p>
+          <br />
+          <br />
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+          >
+            I consider myself a <strong>Free Internet Activist</strong>,
             passionate about the future of the internet, especially privacy,
             free access to information, and{" "}
             <strong>finding European alternatives to Big Tech</strong>.
-            <br />
-            <br />
+          </motion.p>
+          <br />
+          <br />
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6 }}
+          >
             When I’m away from the screen, you’ll find me hiking through nature,
             exploring the outdoors, and tending to my plants.
-            <br />
-            <br />
-            <br />
+          </motion.p>
+          <br />
+          <br />
+          <br />
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1 }}
+          >
             Currently learning? Linux
-            <br />
+          </motion.p>
+          <br />
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.4 }}
+          >
             Favourite Berliner park? Wulheide
-            <br />
+          </motion.p>
+          <br />
+          <motion.p
+            className="paragraph"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.8 }}
+          >
             Favourite color? Forest green
-            <span>
-              <Tree className={s.projectIcon} color="#013220" />
-            </span>
-          </p>
+          </motion.p>
           {/* Come si creano i link in nextjs? */}
           <br />
           <br />
           <div className={s.linksContainer}>
-            <a className={s.link}>Blog</a>
-            <a
+            <motion.a
               className={s.link}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 2.2 }}
+            >
+              Blog
+            </motion.a>
+            <motion.a
+              className={s.link}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 2.6 }}
               href="https://nadiaguarracino.vercel.app/about"
               target="_blank"
             >
               My CV website
-            </a>
+            </motion.a>
           </div>
         </section>
 
@@ -74,59 +142,7 @@ export default function Home() {
                 className={s.card}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ delay: 0.1 }}
-              >
-                <div className={s.cardHeader}>
-                  <LeafIcon className={s.projectIcon} color="#013220" />
-                  <h3 className="h2">Project 1</h3>
-                </div>
-                <p className="paragraph">blbablabalba</p>
-              </motion.div>
-
-              <motion.div
-                className={s.card}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2 }}
-              >
-                <div className={s.cardHeader}>
-                  <LeafIcon className={s.projectIcon} color="#013220" />
-                  <h3 className="h2">Project 1</h3>
-                </div>
-                <p className="paragraph">blbablabalba</p>
-              </motion.div>
-
-              <motion.div
-                className={s.card}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
                 transition={{ delay: 0.3 }}
-              >
-                <div className={s.cardHeader}>
-                  <LeafIcon className={s.projectIcon} color="#013220" />
-                  <h3 className="h2">Project 1</h3>
-                </div>
-                <p className="paragraph">blbablabalba</p>
-              </motion.div>
-
-              <motion.div
-                className={s.card}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.4 }}
-              >
-                <div className={s.cardHeader}>
-                  <LeafIcon className={s.projectIcon} color="#013220" />
-                  <h3 className="h2">Project 1</h3>
-                </div>
-                <p className="paragraph">blbablabalba</p>
-              </motion.div>
-
-              <motion.div
-                className={s.card}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.5 }}
               >
                 <div className={s.cardHeader}>
                   <LeafIcon className={s.projectIcon} color="#013220" />
@@ -152,33 +168,85 @@ export default function Home() {
                 className={s.card}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ delay: 0.7 }}
-              >
-                <div className={s.cardHeader}>
-                  <LeafIcon className={s.projectIcon} color="#013220" />
-                  <h3 className="h2">Project 1</h3>
-                </div>
-                <p className="paragraph">blbablabalba</p>
-              </motion.div>
-
-              <motion.div
-                className={s.card}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.8 }}
-              >
-                <div className={s.cardHeader}>
-                  <LeafIcon className={s.projectIcon} color="#013220" />
-                  <h3 className="h2">Project 1</h3>
-                </div>
-                <p className="paragraph">blbablabalba</p>
-              </motion.div>
-
-              <motion.div
-                className={s.card}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
                 transition={{ delay: 0.9 }}
+              >
+                <div className={s.cardHeader}>
+                  <LeafIcon className={s.projectIcon} color="#013220" />
+                  <h3 className="h2">Project 1</h3>
+                </div>
+                <p className="paragraph">blbablabalba</p>
+              </motion.div>
+
+              <motion.div
+                className={s.card}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.2 }}
+              >
+                <div className={s.cardHeader}>
+                  <LeafIcon className={s.projectIcon} color="#013220" />
+                  <h3 className="h2">Project 1</h3>
+                </div>
+                <p className="paragraph">blbablabalba</p>
+              </motion.div>
+
+              <motion.div
+                className={s.card}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.5 }}
+              >
+                <div className={s.cardHeader}>
+                  <LeafIcon className={s.projectIcon} color="#013220" />
+                  <h3 className="h2">Project 1</h3>
+                </div>
+                <p className="paragraph">blbablabalba</p>
+              </motion.div>
+
+              <motion.div
+                className={s.card}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.8 }}
+              >
+                <div className={s.cardHeader}>
+                  <LeafIcon className={s.projectIcon} color="#013220" />
+                  <h3 className="h2">Project 1</h3>
+                </div>
+                <p className="paragraph">blbablabalba</p>
+              </motion.div>
+
+              <motion.div
+                className={s.card}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 2.1 }}
+              >
+                <div className={s.cardHeader}>
+                  <LeafIcon className={s.projectIcon} color="#013220" />
+                  <h3 className="h2">Project 1</h3>
+                </div>
+                <p className="paragraph">blbablabalba</p>
+              </motion.div>
+
+              <motion.div
+                className={s.card}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 2.4 }}
+              >
+                <div className={s.cardHeader}>
+                  <LeafIcon className={s.projectIcon} color="#013220" />
+                  <h3 className="h2">Project 1</h3>
+                </div>
+                <p className="paragraph">blbablabalba</p>
+              </motion.div>
+
+              <motion.div
+                className={s.card}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 2.7 }}
               >
                 <div className={s.cardHeader}>
                   <LeafIcon className={s.projectIcon} color="#013220" />
