6c97379833f6a04cbfd6f9304f1da990e0d750b3
Nadia Guarracino <nad.guar@gmail.com>
2025-04-14 17:50:27 +0200

    adjust image + about copy

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -9,17 +9,33 @@ import Image from "next/image";
 export default function AboutSection() {
   return (
     <div className={s.container}>
-      <Image src="/profile.jpg" alt="Profile" width={100} height={100} />
+      <Image src="/profile.jpg" alt="Profile" width={200} height={200} />
       <motion.h1
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.1 }}
         className={t.h1}
       >
+        <br />
         Nadia Guarracino
       </motion.h1>
 
-      <h3>Full-Stack Engineer @Doctolib</h3>
+      <h2>Full-Stack Engineer @Doctolib</h2>
+      <br />
+      <p>
+        I have been building for the web since 2019, and my current stack
+        includes React, Ruby on Rails, and PostgreSQL.
+      </p>
+      <br />
+      <p>
+        In this blog, I share what I learn, practical experiences, real-world
+        solutions, and interesting resources related to web development.
+      </p>
+      <br />
+      <p>
+        This is a space to document my journey — and hopefully help other
+        developers along the way{" "}
+      </p>
     </div>
   );
 }
