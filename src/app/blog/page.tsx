f78f59d4eb59ad8c04cbefd941a1f2620510df20
Nadia Guarracino <nad.guar@gmail.com>
2025-04-22 22:19:12 +0200

    add home link to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,14 @@
 import { ArticleCards } from "@/app/components/ArticleCards";
 import t from "@/app/style/typography.module.css";
+import l from "@/app/style/layout.module.css";
 import s from "./Page.module.css";
+import "@/app/style/globals.css";
 import Label from "../components/Label";
 import { Topic } from "../types";
+import Link from "next/link";
+import Home from "@/app/icons/Home";
+import LeftArrow from "@/app/icons/LeftArrow";
+import clsx from "clsx";
 
 export const metadata = {
   title: "Blog",
@@ -11,11 +17,17 @@ export const metadata = {
 };
 
 export default function Page() {
-  const topics = ["js", "cs", "db", "docker", "ts", "ruby", "ror", "react"];
+  const topics = ["js", "cs", "db", "devops", "ts", "ruby", "ror", "react"];
 
   return (
     <div className={s.pageLayout}>
-      <h1 className={t.blogTitle}>Blog</h1>
+      <div className={clsx(l.flex, l.aic)}>
+        <Link href="/" className={clsx(l.flex, l.aic)}>
+          <LeftArrow className="icon" />
+          <Home className="icon" />
+        </Link>
+        <h1 className={t.blogTitle}>Blog</h1>
+      </div>
       {/* TODO: move lavelContainer to a separate component */}
       <div className={s.labelContainter}>
         {topics.map((topic) => (
