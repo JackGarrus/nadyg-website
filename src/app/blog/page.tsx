edb366265eb649ee3b229272edbff917d52ef898
Nadia Guarracino <nad.guar@gmail.com>
2025-04-22 21:02:06 +0200

    add labels to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,26 @@
 import { BlogPosts } from "@/app/components/posts";
 import t from "@/app/style/typography.module.css";
 import s from "./Page.module.css";
+import Label from "../components/Label";
+import { Topic } from "../types";
 
 export const metadata = {
   title: "Blog",
+  // TODO: change description
   description: "Read my blog.",
 };
 
 export default function Page() {
+  const topics = ["js", "cs", "db", "docker", "ts", "ruby", "ror", "react"];
+
   return (
     <div className={s.pageLayout}>
       <h1 className={t.blogTitle}>Blog</h1>
+      <div className={s.labelContainter}>
+        {topics.map((topic) => (
+          <Label key={topic} topic={topic as Topic} />
+        ))}
+      </div>
       <BlogPosts />
     </div>
   );
