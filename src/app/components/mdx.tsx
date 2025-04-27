311a7df0df04f066bfbe9faf4d985e475c40a6de
Nadia Guarracino <nad.guar@gmail.com>
2025-04-27 21:00:58 +0200

    make images in article centered

--- src/app/components/mdx.tsx
+++ src/app/components/mdx.tsx
@@ -37,7 +37,11 @@ function CustomLink(props) {
 }
 
 function ResponsiveImage(props) {
-  return <Image alt={props.alt} {...props} />;
+  return (
+    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <Image alt={props.alt} {...props} />
+    </div>
+  );
 }
 
 function Code({ children, ...props }) {
@@ -60,7 +64,7 @@ function slugify(str: string) {
     .replace(/\-\-+/g, "-");
 }
 
-function createHeading(level: number) {
+function heading(level: number) {
   const Heading = ({ children }: { children: ReactNode }) => {
     const slug = slugify(children);
 
@@ -93,7 +97,7 @@ function createHeading(level: number) {
   return Heading;
 }
 
-function createParagraph({ children }: { children: ReactNode }) {
+function paragraph({ children }: { children: ReactNode }) {
   return (
     <>
       <p className={clsx(t.p, t.blogPostParagraph)}>{children}</p>
@@ -126,10 +130,10 @@ function callout({ children }: { children: ReactNode }) {
 }
 
 const components = {
-  h1: createHeading(1),
-  h2: createHeading(2),
-  h3: createHeading(3),
-  p: createParagraph,
+  h1: heading(1),
+  h2: heading(2),
+  h3: heading(3),
+  p: paragraph,
   strong: Strong,
   blockquote: Blockquote,
   ul: UnorderedList,
